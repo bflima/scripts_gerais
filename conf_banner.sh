77fa57d4b4374437dba8c1d9a0c8785689814768
Bruno Lima <47477458+bflima@users.noreply.github.com>
2024-04-18 13:01:45 -0300

    Update conf_banner.sh

--- conf_banner.sh
+++ conf_banner.sh
@@ -1,1 +1,22 @@
+#!/usr/bin/env/bash
 
+## INFO ##
+## NOME.............: conf_banner.sh
+## VERSÃO...........: 1.0
+## DESCRIÇÃO........: Atualiza o banner do sistema removendo as informações
+## DATA DA CRIAÇÃO..: 18/04/2024
+## ESCRITO POR......: Bruno Lima
+## E-MAIL...........: bruno@lc.tec.br
+## DISTRO...........: Debian GNU/Linux 12
+## LICENÇA..........: GPLv3
+## Git Hub..........: https://github.com/bflima
+
+# Declaração de variáveis
+ISSUE=$(find /etc/ -iname issue)
+ISSUENET=$(find /etc/ -iname issue.net)
+MOTD=$(find /etc/ -iname motd)
+MSG_BANNER='Acesso ao sistema monitorado'
+
+echo "$MSG_BANNER" > "$ISSUE"
+echo "$MSG_BANNER" > "$ISSUENET"
+echo "$MSG_BANNER" > "$MOTD"
